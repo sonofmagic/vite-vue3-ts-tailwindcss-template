fa2f50cb4e959d2f3e9fde337b827cc82b579026
ice breaker <1324318532@qq.com>
2024-06-13 14:07:59 +0800

    capture go

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
@@ -8,11 +8,40 @@ const count = ref(0)
 function inc() {
   count.value++
 }
+
+const buttonRef = ref<HTMLButtonElement>()
+
+onMounted(() => {
+  window.addEventListener('click', () => {
+    console.log('---window--capture--false-------')
+  })
+  window.addEventListener('click', () => {
+    console.log('---window--capture--true-------')
+  }, true)
+  document.addEventListener('click', () => {
+    console.log('---document--capture--false-------')
+  })
+  document.addEventListener('click', () => {
+    console.log('---document--capture--true-------')
+  }, true)
+  buttonRef.value?.addEventListener('click', () => {
+    console.log('---button--capture--true-------')
+  }, true)
+  buttonRef.value?.addEventListener('click', () => {
+    console.log('---button--capture--false-------')
+  })
+})
+
+onBeforeUnmount(() => {
+
+})
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
+  <!-- onMounted -->
+  <button ref="buttonRef" class="xxx">
+    撒旦艰苦拉萨看来打开链接撒
+  </button>
   <div class="card">
     <button v-throttle type="button" @click="count++" @dblclick="count += 2">
       raw count is {{ count }}
